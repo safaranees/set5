e8f739a7d7a9d5491eb79c0e812ae6bb7905ca67
safaranees <aneesparvin08@gmail.com>
2019-03-18 10:10:28 +0530

    Create Concatenate.java

---
+++ Concatenate.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Concatenate
+{
+  public static void main(String args[])
+  {
+    Scanner str=new Scanner(System.in);
+    String str1=str.next();
+    String str2=str.next();
+    System.out.println(str1+str2);
+  }
+}
